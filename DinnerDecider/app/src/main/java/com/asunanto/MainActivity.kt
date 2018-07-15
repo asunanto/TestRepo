844be819035def2ecd3c773c0d0e1f927dafede5
unknown <wibowo.sutanto91@gmail.com>
2018-07-15 16:30:06 +1000

    Dinner decider app main file

---
+++ DinnerDecider/app/src/main/java/com/asunanto/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.asunanto.dinnerdecider
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    val FoodList = arrayListOf("Chinese","Japanese","Italian","White Food","Thai")
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        DecideButton.setOnClickListener{
+            val Random = Random()
+            val RandomFood = Random.nextInt(FoodList.count())
+            SelectedFoodText.text = FoodList[RandomFood]
+        }
+        AddFoodButton.setOnClickListener {
+            val NewFood = AddFoodText.text.toString()
+            FoodList.add(NewFood)
+            AddFoodText.text.clear()
+            println(FoodList)
+        }
+    }
+}
